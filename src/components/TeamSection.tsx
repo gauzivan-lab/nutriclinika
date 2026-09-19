1f263d9ae1718b9f73c2586aa20efa8902b0cb87
Иван Войтихович <ivangauz@MacBook-Air-Ivan-3.local>
2026-09-19 18:40:36 +0300

    Slightly lower TeamSection text block to reveal more of team photo

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -15,7 +15,7 @@ const TeamSection: React.FC = () => {
   };
 
   return (
-    <section className="relative min-h-[90vh] sm:min-h-screen flex items-end">
+    <section className="relative min-h-[95vh] sm:min-h-[112vh] flex items-end">
       {/* Background image with overlay */}
       <div className="absolute inset-0 z-0">
         <img
